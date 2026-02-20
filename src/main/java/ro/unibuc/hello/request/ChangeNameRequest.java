beb0c0ee76a3d4fa544cf6d92bafd4bcaebf2aeb
Radu Dumitru <radu.dumitru@rocketmail.com>
2026-02-20 19:09:35 +0200

    Renamed package hello to prodeng

--- src/main/java/ro/unibuc/hello/request/ChangeNameRequest.java
+++
@@ -1,8 +0,0 @@
-package ro.unibuc.hello.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ChangeNameRequest(
-    @NotBlank(message = "Name is required")
-    String name
-) {}
--- src/main/java/ro/unibuc/hello/request/EditTodoRequest.java
+++
@@ -1,8 +0,0 @@
-package ro.unibuc.hello.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record EditTodoRequest(
-    @NotBlank(message = "Description is required")
-    String description
-) {}
