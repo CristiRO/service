22f7752c3dba222c50a68d1cde48e031ee9af6de
Radu Dumitru <radu.dumitru@rocketmail.com>
2026-02-20 19:17:30 +0200

    Rename hello package to prodeng via IDE

--- src/main/java/ro/unibuc/hello/response/TodoResponse.java
+++
@@ -1,9 +0,0 @@
-package ro.unibuc.hello.response;
-
-public record TodoResponse(
-    String id,
-    String description,
-    boolean done,
-    String assigneeName,
-    String assigneeEmail
-) {}
